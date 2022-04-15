7e01b7f3f3ba00d620968c6de193ce7e0553e08d
tylerdowney13 <tylerdowney83@gmail.com>
2022-04-15 14:55:08 -0230

    Added CORS

--- src/main/java/com/S4FinalSprint/CustomerAPI/domain/CustomerRepository.java
+++ src/main/java/com/S4FinalSprint/CustomerAPI/domain/CustomerRepository.java
@@ -3,9 +3,11 @@ package com.S4FinalSprint.CustomerAPI.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findByFirstName(@Param("firstName") String firstName);
--- src/main/java/com/S4FinalSprint/CustomerAPI/domain/UserRepository.java
+++ src/main/java/com/S4FinalSprint/CustomerAPI/domain/UserRepository.java
@@ -3,9 +3,11 @@ package com.S4FinalSprint.CustomerAPI.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUsername(@Param("username") String username);
